df238b3dfbbca0d85424e46c35ab02fc54b93adf
Kojo Nhyira Mante-Dankwa <97101203+Kojonhyiraa@users.noreply.github.com>
2025-01-22 18:42:36 +0000

    Init

--- src/main/java/kttech/QrCodeGenerator/repository/UserRepository.java
+++ src/main/java/kttech/QrCodeGenerator/repository/UserRepository.java
@@ -1,4 +1,11 @@
 package kttech.QrCodeGenerator.repository;
 
-public interface UserRepository {
+import kttech.QrCodeGenerator.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+
+
 }
