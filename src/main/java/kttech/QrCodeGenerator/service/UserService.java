2dea0e918ca91c9471f90f4fa3e8b6316225fdf0
Kojo Nhyira Mante-Dankwa <97101203+Kojonhyiraa@users.noreply.github.com>
2025-01-22 18:42:42 +0000

    Init

--- src/main/java/kttech/QrCodeGenerator/service/UserService.java
+++ src/main/java/kttech/QrCodeGenerator/service/UserService.java
@@ -1,8 +1,9 @@
 package kttech.QrCodeGenerator.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface UserService {
-    private
+
 }
