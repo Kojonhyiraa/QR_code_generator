2511548ffab41c4388ab26b4cc3d9dffbbda7fcf
Kojo Nhyira Mante-Dankwa <97101203+Kojonhyiraa@users.noreply.github.com>
2025-01-22 18:46:16 +0000

    Update

--- src/main/java/kttech/QrCodeGenerator/service/UserService.java
+++ src/main/java/kttech/QrCodeGenerator/service/UserService.java
@@ -1,9 +1,14 @@
 package kttech.QrCodeGenerator.service;
 
+import kttech.QrCodeGenerator.models.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public interface UserService {
+
+    public List<User> getAllUsers();
 
 }
