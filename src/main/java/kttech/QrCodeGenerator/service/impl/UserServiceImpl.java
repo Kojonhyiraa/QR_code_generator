305906d8c64c56ac1c49fbcd6d160ebd15987dd5
Kojo Nhyira Mante-Dankwa <97101203+Kojonhyiraa@users.noreply.github.com>
2025-01-23 22:44:37 +0000

    Update

--- src/main/java/kttech/QrCodeGenerator/service/impl/UserServiceImpl.java
+++ src/main/java/kttech/QrCodeGenerator/service/impl/UserServiceImpl.java
@@ -14,10 +14,22 @@ public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
 
+    @Override
     public List <User> getAllUsers() {
-
         return userRepository.findAll();
     }
+
+    @Override
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User findUserById(Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+    }
+
 
 
 
