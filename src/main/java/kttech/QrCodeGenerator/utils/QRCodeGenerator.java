e62ad243de7dd3cc4c35373803f2d6ae2f1432e2
Kojo Nhyira Mante-Dankwa <97101203+Kojonhyiraa@users.noreply.github.com>
2025-01-25 23:30:04 +0000

    Update

--- src/main/java/kttech/QrCodeGenerator/utils/QRCodeGenerator.java
+++ src/main/java/kttech/QrCodeGenerator/utils/QRCodeGenerator.java
@@ -1,13 +1,18 @@
 package kttech.QrCodeGenerator.utils;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
 import kttech.QrCodeGenerator.models.User;
 
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+
 
 public class QRCodeGenerator {
 
-    public static void generateQRCode(User user){
+    public static void generateQRCode(User user) throws WriterException {
 
         //Path where we will save the generated QR code image
         String qrCodePath = "";
@@ -18,10 +23,17 @@ public class QRCodeGenerator {
         //Add dependency for QR code generator.Its called zxing
         var qrCodeQwriter = new QRCodeWriter();
 
-        BitMatrix bitMatrix ;
+        BitMatrix bitMatrix = qrCodeQwriter.encode(
+                "ID:"+user.getId() +"\n"+
+                        "First Name:"+user.getFirstName() +"\n"+
+                        "Last Name:"+user.getLastName() +"\n"+
+                        "Email:"+user.getEmail() +"\n"+
+                        "Mobile Number:"+user.getMobileNumber() +"\n",
+                BarcodeFormat.QR_CODE,
+                400,
+                400 ) ;
 
-
+        Path path = FileSystems.getDefault().getPath(qrCodeName);
 
     }
-
 }
