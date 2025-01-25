12fb5cdeff0105e2879bb3399edc8934003526d8
Kojo Nhyira Mante-Dankwa <97101203+Kojonhyiraa@users.noreply.github.com>
2025-01-25 23:50:01 +0000

    Update

--- src/main/java/kttech/QrCodeGenerator/utils/QRCodeGenerator.java
+++ src/main/java/kttech/QrCodeGenerator/utils/QRCodeGenerator.java
@@ -1,9 +1,9 @@
 package kttech.QrCodeGenerator.utils;
 
 import com.google.zxing.BarcodeFormat;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
-import com.google.zxing.datamatrix.DataMatrixReader;
 import com.google.zxing.qrcode.QRCodeWriter;
 import kttech.QrCodeGenerator.models.User;
 
@@ -16,7 +16,7 @@ public class QRCodeGenerator {
     public static void generateQRCode(User user) throws WriterException {
 
         //Path where we will save the generated QR code image
-        String qrCodePath = "";
+        String qrCodePath = "src/main/resources/static";
         //Name of the saved image
         String qrCodeName = qrCodePath+user.getFirstName()+user.getId()+"-QRCode.png";
 
