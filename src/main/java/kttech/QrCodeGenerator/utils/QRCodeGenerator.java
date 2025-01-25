15f2bd6fccaae54d0a1c31fbd7f194cc69b87330
Kojo Nhyira Mante-Dankwa <97101203+Kojonhyiraa@users.noreply.github.com>
2025-01-25 23:19:32 +0000

    Init

--- src/main/java/kttech/QrCodeGenerator/utils/QRCodeGenerator.java
+++ src/main/java/kttech/QrCodeGenerator/utils/QRCodeGenerator.java
@@ -1,4 +1,22 @@
 package kttech.QrCodeGenerator.utils;
 
+import kttech.QrCodeGenerator.models.User;
+import lombok.var;
+
 public class QRCodeGenerator {
+
+    public static void generateQRCode(User user){
+
+        //Path where we will save the generated QR code image
+        String qrCodePath = "";
+        //Name of the saved image
+        String qrCodeName = qrCodePath+user.getFirstName()+user.getId()+"-QRCode.png";
+
+
+        //Add dependency for QR code generator.Its called zxing
+        var qr
+
+
+    }
+
 }
