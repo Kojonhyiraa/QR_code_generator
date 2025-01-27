9381dce96dffba84fda16b644957fafab102322c
Kojo Nhyira Mante-Dankwa <97101203+Kojonhyiraa@users.noreply.github.com>
2025-01-27 13:59:23 +0000

    Fixed bugs

--- src/main/java/kttech/QrCodeGenerator/controller/UserController.java
+++ src/main/java/kttech/QrCodeGenerator/controller/UserController.java
@@ -1,11 +1,14 @@
 package kttech.QrCodeGenerator.controller;
 
+import com.google.zxing.WriterException;
 import kttech.QrCodeGenerator.models.User;
 import kttech.QrCodeGenerator.service.UserService;
+import kttech.QrCodeGenerator.utils.QRCodeGenerator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -16,7 +19,15 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping("/all")
-    public ResponseEntity<List<User>> getUsers() {
+    public ResponseEntity<List<User>> getUsers() throws IOException, WriterException {
+        //Get all records in the database
+        List<User> users = userService.getAllUsers();
+        //For each record in the database generate a QR code
+        if (!users.isEmpty()) {
+            for(User user : users) {
+                QRCodeGenerator.generateQRCode(user);
+            }
+        }
         return ResponseEntity.ok(userService.getAllUsers());
     }
 
