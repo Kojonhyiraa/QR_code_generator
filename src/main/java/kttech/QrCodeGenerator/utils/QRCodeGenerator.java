5e0ee831b2e25a4835be50e1de012c7800f922b6
Kojo Nhyira Mante-Dankwa <97101203+Kojonhyiraa@users.noreply.github.com>
2025-01-27 15:23:48 +0000

    Final

--- src/main/java/kttech/QrCodeGenerator/utils/QRCodeGenerator.java
+++ src/main/java/kttech/QrCodeGenerator/utils/QRCodeGenerator.java
@@ -5,11 +5,14 @@ import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
+import com.google.zxing.EncodeHintType;
 import kttech.QrCodeGenerator.models.User;
 
 import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
 
 public class QRCodeGenerator {
     public static void generateQRCode(User user) throws WriterException, IOException {
@@ -18,9 +21,9 @@ public class QRCodeGenerator {
 
         var qrCodeWriter = new QRCodeWriter();
 
-        // Simplify the QR code content
+        // Format user data in column format with URL-encoded newlines
         String qrContent = String.format(
-                "{\"id\":%d,\"firstName\":\"%s\",\"lastName\":\"%s\",\"email\":\"%s\",\"mobileNumber\":\"%s\"}",
+                "{  id: \"%d\"   ,    firstName: \" %s \"   ,    lastName: \"%s\"   ,    email : \"%s\"   ,    mobileNumber:\"%s\"  }",
                 user.getId(),
                 user.getFirstName(),
                 user.getLastName(),
@@ -30,11 +33,16 @@ public class QRCodeGenerator {
 
         System.out.println("QR Code Content: " + qrContent); // Debugging
 
+        // Set error correction level to HIGH
+        Map<EncodeHintType, Object> hints = new HashMap<>();
+        hints.put(EncodeHintType.ERROR_CORRECTION, "H"); // H = High error correction
+
         BitMatrix bitMatrix = qrCodeWriter.encode(
                 qrContent,
                 BarcodeFormat.QR_CODE,
-                600, // Increased size
-                600
+                600,
+                600,
+                hints
         );
 
         Path path = FileSystems.getDefault().getPath(qrCodeName);
